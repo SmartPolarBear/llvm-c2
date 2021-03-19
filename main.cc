82ed1030a950ca9e4c2c9cbdbc664dfd206c7a6b
SmartPolarBear <clevercoolbear@outlook.com>
2021-03-19 21:37:34 +0800

    CLion

--- main.cc
+++ main.cc
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <string_view>
 
 using namespace std;
 
+static string_view<char> self_path{""};
+
 int main(int argc, char **argv)
 {
+    self_path = argv[0];
     return 0;
 }
