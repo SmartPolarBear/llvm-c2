32cec1fc8b9ddb7bea552ad902c8dfc863c136df
SmartPolarBear <clevercoolbear@outlook.com>
2021-03-28 23:06:42 +0800

    add facility for IR generating

--- include/Program.h
+++ include/Program.h
@@ -4,13 +4,28 @@
 #include "Node.h"
 #include "Block.h"
 
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Verifier.h>
+
 #include <vector>
 #include <memory>
 
 class Program : public Singleton<Program>
 {
   public:
-    Program() = default;
+    Program()
+        : context_(std::make_unique<llvm::LLVMContext>()),
+          module_(std::make_unique<llvm::Module>("llvm-c2", *context_)),
+          ir_builder_(std::make_unique<llvm::IRBuilder<>>(*context_))
+    {
+    }
+
+    Program(const Program &) = delete;
+    Program(Program &&) = delete;
+    Program &operator=(const Program &) = delete;
     ~Program() = default;
 
     void SetRoot(Parser::AST::Block *root)
@@ -18,8 +33,12 @@ class Program : public Singleton<Program>
         root_ = root;
     }
 
-    
+    void Generate();
 
   private:
     Parser::AST::Block *root_;
+
+    std::unique_ptr<llvm::LLVMContext> context_;
+    std::unique_ptr<llvm::Module> module_;
+    std::unique_ptr<llvm::IRBuilder<>> ir_builder_;
 };
